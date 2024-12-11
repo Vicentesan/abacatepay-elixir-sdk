54bc5d8d07d4d6371e6ea4b3397fd9849f3ab77d
Vicentesan <vikom.sanchez@gmail.com>
2024-12-11 16:21:49 -0300

    chore: add package license, changelog and readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# AbacatePay SDK
+
+<!-- [![Hex.pm](https://img.shields.io/hexpm/v/abacate_pay.svg)](https://hex.pm/packages/abacate_pay) -->
+<!-- [![Documentation](https://img.shields.io/badge/documentation-gray)](https://hexdocs.pm/abacate_pay) -->
+
+Elixir SDK for integrating with AbacatePay's payment services.
+
+## Installation
+
+<!-- Add `abacate_pay` to your list of dependencies in `mix.exs`:  -->
+Not available yet.
+
+## Features
+
+- Customer Management
+  - Create customers
+  - List customers
+- Billing Operations
+  - Create billings
+  - List billings
--- mix.exs
+++ mix.exs
@@ -1,13 +1,21 @@
-defmodule Abacatepay.MixProject do
+defmodule AbacatePay.MixProject do
   use Mix.Project
+
+  @version "0.0.0"
+  @source_url "https://github.com/vicentesan/abacatepay-elixir-sdk"
 
   def project do
     [
-      app: :abacatepay,
-      version: "0.0.0",
-      elixir: "~> 1.14",
+      app: :abacate_pay,
+      version: @version,
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      docs: docs(),
+      name: "AbacatePay",
+      source_url: @source_url
     ]
   end
 
@@ -21,7 +29,47 @@ defmodule Abacatepay.MixProject do
     [
       {:jason, "~> 1.4"},
       {:httpoison, "~> 2.0"},
-      {:ex_doc, "~> 0.29", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp description do
+    """
+    AbacatePay SDK - A payment processing library for integrating with AbacatePay's payment services.
+    """
+  end
+
+  defp package do
+    [
+      name: "abacate_pay",
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE* CHANGELOG*),
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => @source_url,
+        "Changelog" => "#{@source_url}/blob/master/CHANGELOG.md"
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_ref: "v#{@version}",
+      # canonical: "http://hexdocs.pm/abacate_pay",
+      extras: ["README.md", "CHANGELOG.md"],
+      groups_for_modules: [
+        "Resources": [
+          AbacatePay.Resources.Customer,
+          AbacatePay.Resources.Customers,
+          AbacatePay.Resources.Billing
+        ],
+        "Types": [
+          AbacatePay.Types.Customer,
+          AbacatePay.Types.Billing,
+          AbacatePay.Types.Product
+        ]
+      ]
     ]
   end
 end 
